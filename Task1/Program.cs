8231dbe4d9aa9c0604ebdbce90302ac5f5e2d8d0
Sergey Dyatchin <s.dyatchin1983@gmail.com>
2024-02-07 01:39:36 +0300

    the first task has been solved

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Условие:
+// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+bool IsAllDigits(string enterLine ) // Функция проверки, что ввели число.
+{
+    foreach (char symbol in enterLine)
+    {
+        if (!char.IsDigit(symbol))  
+            return false;
+    }
+    return true;
+}
+
+int RequestingNumber() // функция запроса у пользователя числа.
+{
+    string enterLine;
+    int number;
+    Console.Write("Введите целое неотрицательное число:");
+    while (true)
+    {
+        enterLine = Console.ReadLine()!; // NULL проверка в следующей строке
+        if (IsAllDigits(enterLine)&& enterLine!="")
+        {
+            number = Convert.ToInt32(enterLine);
+            return number;
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели некоректное значение, повторите ввод!");
+        }
+    }
+}
+
+void ShowNumbers(int start, int end) //Функция печати чисел
+{
+    if (start == end) // Базовый случай
+    {
+        if (start != 0) Console.Write(start); // Исключаем печать "0", так как он не является натуральным числом.
+        return; 
+    }
+    if (start != 0) Console.Write(start + " "); // Исключаем печать "0", так как он не является натуральным числом.
+    ShowNumbers(start + 1, end);
+}
+
+int firstNumber = RequestingNumber();
+int secondNumber = RequestingNumber();
+
+if (firstNumber < secondNumber)
+{
+    ShowNumbers(firstNumber, secondNumber);
+}
+else
+{
+    ShowNumbers(secondNumber,firstNumber);
+}
